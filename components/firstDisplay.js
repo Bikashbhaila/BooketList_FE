b494d7a99f26b5420f21efd932655d9b0de59608
Adrian <perkadot@hotmail.com>
2021-07-22 13:09:46 -0700

    Created and refactored display containers into separate files

---
+++ components/firstDisplay.js
@@ -0,0 +1,61 @@
+//First book display fetch
+window.onload = getFirstDisplay();
+
+function getFirstDisplay() {
+  // google books api link
+  const URL = `https://www.googleapis.com/books/v1/volumes?q=harry+potter&limit=12`;
+
+  // fetch data form the api, parse into json and store as books
+  fetch(URL)
+    .then((response) => response.json())
+    .then((res) => {
+      const books = res.items;
+      document.getElementById("first-display").innerHTML = ""; // resetting container so it will load the content in slow networks
+      firstDisplay(books); // delegating to display books in results container
+    })
+    .catch((err) => console.log(err));
+}
+//First book display container
+function firstDisplay(books) {
+  console.log("firstDisplayFunc:", books);
+  for (const book of books) {
+    const title = book.volumeInfo.title;
+    const author = book.volumeInfo.authors;
+    const isbn =
+      book.volumeInfo.industryIdentifiers[0].identifier +
+      " , " +
+      book.volumeInfo.industryIdentifiers[1].identifier;
+    const image_url = book.volumeInfo.imageLinks.thumbnail.replace(
+      "http",
+      "https"
+    );
+    let categories = book.volumeInfo.categories;
+    if (categories === undefined) {
+      categories = "Young Adult Fiction";
+    } else {
+      categories = book.volumeInfo.categories[0];
+    }
+
+    // create card components
+    const cardDiv = document.createElement("div");
+    cardDiv.classList.add("col-sm-3");
+
+    cardDiv.innerHTML = `
+    <div class="card">
+      <img src=${image_url} class="card-img-top height="300" width="250" alt=${title}>
+      <div class="card-body">
+        <h5 class="card-title">${title}</h5>
+        <h6 class="sub-title text-muted">${author}</h6>
+        <h6 class="sub-title text-muted">${isbn}</h6>
+        <p class="card-text">${categories}</p>  
+        <button name="add_Button strong" class="btn btn-primary">BOOK ET</button>
+      </div>
+    </div>
+    `;
+    // append card to results container to display results
+    document.getElementById("first-display").appendChild(cardDiv);
+
+    const addBtn = cardDiv.firstElementChild.children[1].lastElementChild;
+    addBtn.addEventListener("click", addCard);
+  }
+}
